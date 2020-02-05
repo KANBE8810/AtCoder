26e239e8641877baa10c8385701ec469ff922c5e
KANBE8810 <s19C1034BQ@s.chibakoudai.jp>
2020-02-05 20:31:17 +0900

    AC

---
+++ A/A_143.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+
+    if(A <= B*2)
+        cout << 0 << endl;
+    else
+        cout << A-B*2 << endl;
+
+}
